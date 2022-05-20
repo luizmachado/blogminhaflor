e5dec599aff4c945814f68778fd1ad1311f384e3
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-05-20 19:37:03 -0300

    Configure essential View to get index working

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from .models import Post
+from comentarios.models import Comment
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    context_object_name = 'posts'
+    paginate_by = 3
 
 
 class PostSearch(PostIndex):
