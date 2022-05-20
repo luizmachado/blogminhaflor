487675c2776abe899f4c15ba626d2adf877ccbe5
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-05-20 00:47:47 -0300

    Install summernote
    Add django_summernote into the list of installed apps

--- blog/settings.py
+++ blog/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'posts',
     'categorias',
     'comentarios',
+    'django_summernote',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
