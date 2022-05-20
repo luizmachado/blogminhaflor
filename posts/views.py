d79a6aae1d6a2633d437ac4c34ff69a5e6f0b3e2
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-05-20 01:07:25 -0300

    Basic view skeleton

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+
+class PostIndex(ListView):
+    pass
+
+
+class PostSearch(PostIndex):
+    pass
+
+
+class PostCategory(PostIndex):
+    pass
+
+
+class PostDetails(UpdateView):
+    pass
