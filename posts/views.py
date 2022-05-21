f624d451cf8e15e83c2b72410b415728a9cb20f0
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-05-20 23:25:16 -0300

    Fix comment counter
    Fix number of comments per post, which was counting every comment
    including those which weren't published

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
 from comentarios.models import Comment
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
@@ -10,6 +11,19 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     context_object_name = 'posts'
     paginate_by = 3
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+        # Count only comments published
+        qs = qs.annotate(
+            comment_count=Count(
+                Case(
+                    When(comment__comment_published=True, then=1)
+                )
+            )
+        )
+        return qs
 
 
 class PostSearch(PostIndex):
