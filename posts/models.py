c05d1fbdf27ed06679e76031704d3d3625bccdff
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-05-25 01:04:59 -0300

    Create method to resize images
    Create a method to resize images when post is saved, all images
    uploaded in Post Model are going to be resized to max 800px width.

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Category
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
+from PIL import Image
+from pathlib import Path
 
 
 class Post(models.Model):
@@ -19,3 +22,27 @@ class Post(models.Model):
 
     def __str__(self):
         return self.post_title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.resize_image(self.post_image.name, 800)
+
+
+    @staticmethod
+    def resize_image(image_name, new_width):
+        image_path = Path(settings.MEDIA_ROOT / image_name)
+        img = Image.open(image_path)
+        width, height = img.size
+        if width <= 800:
+            img.close()
+            return
+        new_height = round((height * new_width) / width)
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            image_path,
+            optimize=True,
+            quality=60,
+        )
+        img.close()
+        new_img.close()
